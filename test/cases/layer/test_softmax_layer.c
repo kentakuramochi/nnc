3fb62a4d1fd7ba7a42c77793c4446cff9b480ec9
mts0629 <soratobi93@gmail.com>
2024-09-10 23:59:34 +0900

    Add a test for softmax forward

--- test/cases/layer/test_softmax_layer.c
+++ test/cases/layer/test_softmax_layer.c
@@ -40,3 +40,27 @@ void test_alloc_and_free(void) {
 
     free_memories(&layer);
 }
+
+void test_forward(void) {
+    Layer layer = {
+        .params={ LAYER_TYPE_SOFTMAX, .batch_size=2, .in=3 }
+    };
+
+    softmax_layer_init(&layer);
+
+    float x[] = {
+        0.8, 0.2, 0.1,
+        0.3, -0.9, 0.4
+    };
+
+    float y[] = {
+        0.488903, 0.268315, 0.242782,
+        0.415565, 0.125166, 0.459270
+    };
+
+    TEST_ASSERT_EQUAL_FLOAT_ARRAY(
+        y, layer.forward(&layer, x), (2 * 3)
+    );
+
+    free_memories(&layer);
+}
